fa73ec87f4e6a987e2109c27dfe82eaaf2d70987
lucas <lucaswtds@gmail.com>
2024-01-24 15:11:47 -0300

    feat: add decrease and increase product quantity in cart reducer

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -25,6 +25,29 @@ const cartReducer = (state = initialState, action) => {
         ...state,
         products: [...state.products, {...action.payload, quantity: 1}]
       }
+    case cartActionTypes.REMOVE_PRODUCT:
+      return {
+        ...state,
+        products: state.products.filter(product => product.id !== action.payload)
+      }
+    case cartActionTypes.INCREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        products: state.products.map(product =>
+          product.id === action.payload
+            ? {...product, quantity: product.quantity + 1}
+            : product
+        )
+      }
+      case cartActionTypes.DECREASE_PRODUCT_QUANTITY:
+        return {
+          ...state,
+          products: state.products.map(product =>
+            product.id === action.payload
+              ? {...product, quantity: product.quantity - 1}
+              : product
+          ).filter(product => product.quantity > 0)
+        }
     default:
       return state
   }
